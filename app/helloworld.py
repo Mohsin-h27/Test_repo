b5858029ef651970ec2b5c689b2206c33ac8f91f
mohsin <mohsin.h@turing.com>
2025-04-03 17:12:40 +0500

    new test cases

--- app/helloworld.py
+++ app/helloworld.py
@@ -1,3 +1,7 @@
 def hello_world():
     a = 5
     print('hello World!')
+
+
+def hello_world2():
+    print('hello world2')
--- tests/test_helloworld2.py
+++ tests/test_helloworld2.py
@@ -4,14 +4,14 @@ import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from app.helloworld import hello_world
+from app.helloworld import hello_world, hello_world2
 
-class TestHelloWorld(unittest.TestCase):
+class TestHelloWorld2(unittest.TestCase):
 
     @patch('builtins.print')
     def test_hello_world(self, mock_print):
         # Call the hello_world function
-        hello_world()
+        hello_world2()
         
         # Check if print was called with the correct argument
-        mock_print.assert_called_with('hello World!')
+        mock_print.assert_called_with('hello world2')
